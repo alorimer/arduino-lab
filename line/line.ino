9d3d42d9f53ab45e9a7906af107fc0eec3a115b7
Andrew Lorimer <andrew@lorimer.id.au>
2017-03-29 14:07:03 +1100

    finalise line tracker (renamed as line.ino)

--- line/line.ino
+++ line/line.ino
@@ -5,8 +5,8 @@ SoftwareSerial mySerial(3, 2);
 Adafruit_GPS GPS(&mySerial); // GPS object called GPS
 #define GPSECHO  false // raw sentences
 
-int latLine = -37.906877;
-int lonLine;
+float latLine = -37.906877;
+float lonLine;
 
 boolean usingInterrupt = true;
 void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
@@ -17,8 +17,8 @@ void setup() {
   Serial.begin(115200);
   GPS.begin(9600);
 
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); // minimum output + GGA (fix data)
-  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY); // minimum output
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); // minimum output + GGA (fix data)
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY); // minimum output
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
 
   // the nice thing about this code is you can have a timer0 interrupt go off
@@ -43,16 +43,7 @@ void setup() {
 
 
 // Interrupt is called once a millisecond, looks for any new GPS data, and stores it
-SIGNAL(TIMER0_COMPA_vect) {
-  char c = GPS.read();
-  // if you want to debug, this is a good time to do it!
-#ifdef UDR0
-  if (GPSECHO)
-    if (c) UDR0 = c;  
-    // writing direct to UDR0 is much much faster than Serial.print 
-    // but only one character can be written at a time. 
-#endif
-}
+SIGNAL(TIMER0_COMPA_vect) { GPS.read();}
 
 void useInterrupt(boolean v) {
   if (v) {
@@ -74,48 +65,28 @@ uint32_t timer = millis();
 
 
 
-void loop()                     // run over and over again
-{
-  // in case you are not using the interrupt above, you'll
-  // need to 'hand query' the GPS, not suggested :(
-  if (! usingInterrupt) {
-    // read data from the GPS in the 'main loop'
-    char c = GPS.read();
-    // if you want to debug, this is a good time to do it!
-    if (GPSECHO)
-      if (c) Serial.print(c);
-  }
-  
-  // if a sentence is received, we can check the checksum, parse it...
-  if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences! 
-    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
-    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
-  
+void loop() {
+  if (GPS.newNMEAreceived()) { // checksum and parse
     if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
       return;  // we can fail to parse a sentence in which case we should just wait for another
   }
-
-  // if millis() or timer wraps around, we'll just reset it
-  if (timer > millis())  timer = millis();
-
+  
   if (millis() - timer > 1000) { 
     timer = millis(); // reset the timer
     if (GPS.fix) {
       Serial.println("    <trkpt lat='" + String(GPS.latitudeDegrees, 6) + "' lon='" + String(GPS.longitudeDegrees, 6) + "'></trkpt>");
        digitalWrite(8, LOW);
-      if (GPS.latitudeDegrees == latLine) {
+      if ((latLine - 0.000100) < GPS.latitudeDegrees < latLine + 0.000100) {
         digitalWrite(5, LOW);
         digitalWrite(6, HIGH);
         digitalWrite(7, LOW);
       }
-      else if (GPS.latitudeDegrees < latLine) {
+      else if (GPS.latitudeDegrees < latLine - 0.000100) {
         digitalWrite(5, HIGH);
         digitalWrite(6, LOW);
         digitalWrite(7, LOW);
       }
-      else if (GPS.latitudeDegrees > latLine) {
+      else if (GPS.latitudeDegrees > latLine + 0.000100) {
          digitalWrite(5, LOW);
         digitalWrite(6, LOW);
         digitalWrite(7, HIGH);
@@ -124,6 +95,9 @@ void loop()                     // run over and over again
     }
     else {
       digitalWrite(8, HIGH);
+      digitalWrite(5, LOW);
+      digitalWrite(6, LOW);
+      digitalWrite(7, LOW);
     }
   }
 }
